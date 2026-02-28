0de3cf3ca8a8cbfc55850fb73b5efb80022f84fa
Yonas <yonas@ekhosphos.com>
2026-02-27 23:19:39 -0800

    postgress, docker work. some frontend done

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,6 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 LOGIN_URL = "accounts:login"
 LOGIN_REDIRECT_URL = "public:index"
