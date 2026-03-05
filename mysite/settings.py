04247b00e0ac99f6553dd3305e87f4f5ef759314
Yonas <yonas@ekhosphos.com>
2026-03-05 00:52:29 -0800

    FrontEndAssetsUptoFonts

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,7 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 LOGIN_URL = "accounts:login"
 LOGIN_REDIRECT_URL = "public:index"
